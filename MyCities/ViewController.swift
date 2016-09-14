c813f1b88d6521b97a1167d58a9f598b9f0c17b6
Cool Dude <cooldude@cools-mac.local>
2016-09-14 18:29:13 +0200

    Initial Commit

---
+++ MyCities/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCities
+//
+//  Created by Cool Dude on 9/14/16.
+//  Copyright © 2016 Organization Name. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
